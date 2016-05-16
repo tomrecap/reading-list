98dd949480ff4b67dfc23aeeafa3bbac7e36d06f
Thomas Carroll <thomas.carroll08@gmail.com>
2016-05-15 21:59:52 -0400

    Replaced the table with a grid & refactored the filter & sorting methods

--- js/app.js
+++ js/app.js
@@ -10,132 +10,78 @@ angular.module('readingListApp', [])
       
       var allBooks = [];
       $scope.books = [];
-      $scope.viewState = '';
       
       $scope.getBooks = getBooks;
-      $scope.showAll = showAll;
-      $scope.showBooksToCheckOut = showBooksToCheckOut;
-      
-      $scope.title = title;
-      $scope.authorFirstName = authorFirstName;
-      $scope.authorLastName = authorLastName;
-      $scope.dateAdded = dateAdded;
-      
+      $scope.filterBooksBy = filterBooksBy;
       
       initialize();
       
       ///////////
       
       function initialize() {
-        getBooks().then(showBooksToCheckOut);
+        getBooks().then(function () {
+          $scope.whichBooksToShow = 'lastTwenty';
+          $scope.sortingProperty = 'author_last';
+        });
       };
       
       function getBooks() {
-        var spreadsheetUrl = 'https://spreadsheets.google.com/feeds/list/1dIssTHJjT3v3iHuQFu6zgH0gcih1T0hGBVxehrV8iJo/od6/public/basic';
+        var spreadsheetUrl = 'https://docs.google.com/spreadsheets/d/1dIssTHJjT3v3iHuQFu6zgH0gcih1T0hGBVxehrV8iJo/pub?gid=983517851&single=true&output=csv';
         
         return $http.get(spreadsheetUrl, {
           params: { alt: 'json'}
         }).then(function (response) {
-          console.log('got a response from google API');
-          allBooks = response.data.feed.entry;
-          
-          console.log('all the data: ');
-          console.log(allBooks);
-        })
+          allBooks = objectsFromCsv(response.data);
+          $scope.books = allBooks;
+        });
       };
       
-      function parseBookData(googleBookObject) {
-        console.log('parsing book object ', googleBookObject);
-        var detailsString = googleBookObject.content.$t;
+      function objectsFromCsv(csvString) {
+        var objects = csvString.split('\n');
+        var keys = objects.shift().split(',');
 
-        if (detailsString == "skip: 0") {
-          console.log('googleBookObject has no content. returning false.')
-          return false;
-        }
-
-        var bookObject = {}
-
-        bookObject.title = googleBookObject.title.$t || '';
-        if (bookObject.title.substr(0,4) == 'Row:') {
-          bookObject.title = '';
-        }
-
-        _.each(detailsString.split(', '), function (pair) {
-          var pieces = pair.split(': ');
-          bookObject[pieces[0]] = pieces[1];
+        objects = _.map(objects, function(string) {
+          var array = string.split(',');
+          var object = { };
+          _(array).forEach(function (value, i) {
+            object[keys[i]] = array[i];
+          });
+          return object;
         });
 
-        bookObject.url = googleBookObject.id.$t;
-
-        console.log('returning usable book object: ', bookObject);
-        return bookObject;
+        return objects;
       };
-      
-      function objectFromString(string) {
-        var object = {};
-        _.each(string.split(', '), function (pair) {
-          var pieces = pair.split(': ');
-          object[pieces[0]] = pieces[1];
-        });
-        return object;
-      }
-      
-      
-      
-      function title(book) {
-        var title = book.title.$t || '';
-        if (title.substr(0,4) == 'Row:') {
-          title = '';
+
+
+      function filterBooksBy(criterion) {
+        var booksToShow = allBooks;
+        
+        if (criterion == 'unread') {
+          booksToShow = _.filter(booksToShow, isAPossibleRead);
+        } else if (criterion == 'lastSixMonths') {
+          var sixMonthsAgo = new Date().setMonth(new Date().getMonth() - 6);
+
+          booksToShow = _(booksToShow)
+            .filter(isAPossibleRead)
+            .filter(function (book) {
+              var addedDate = new Date(book.Timestamp);
+              return addedDate > sixMonthsAgo;
+            }).value();
+        } else if (criterion == 'lastTwenty') {
+          booksToShow = _(booksToShow)
+            .filter(isAPossibleRead)
+            .sortBy('-Timestamp')
+            .take(20)
+            .value();
         }
-        return title;
-      }
-
-      function authorFirstName(book) {
-        return objectFromString(book.content.$t).authorfirst || '';
-      }
-
-      function authorLastName(book) {
-        return objectFromString(book.content.$t).authorlast || '';
-      }
-
-      function dateAdded(book) {
-        return objectFromString(book.content.$t).dateadded || '';
-      }
-      
-      function isAlreadyRead(book) {
-        return !!parseInt(objectFromString(book.content.$t).read || 0);
-      }
-
-      function isAlreadyTried(book) {
-        return !!parseInt(objectFromString(book.content.$t).skip || 0);
+        
+        return function (book) {
+          return _(booksToShow).map('$$hashKey').contains(book.$$hashKey);
+        };
       }
       
       function isAPossibleRead(book) {
-        return !(isAlreadyRead(book) || isAlreadyTried(book));
+        return !(book.read || book.tried);
       }
-      
-      
-      function showAll() {
-        $scope.books = allBooks;
-        $scope.viewState = 'all';
-      }
-
-      function showBooksToCheckOut() {
-        $scope.books = _.filter(allBooks, isAPossibleRead);
-        $scope.viewState = 'possibleReads';
-      }
-      
-      function showAlreadyRead() {
-        $scope.books = _.filter(allBooks, isAlreadyRead);
-      }
-
-      function showAlreadyTried() {
-        $scope.books = _.filter(allBooks, isAlreadyTried);
-      }
-
-      
-      function setToRead(book) {
-        
-      };
     }
-  ]);
+]);
